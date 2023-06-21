ec55e60595095395cc528c5d1811ca4c6e382f02
securitylong100 <30890603+securitylong100@users.noreply.github.com>
2023-06-21 16:38:16 +0700

    update
    updated

---
+++ TTB_PCB_PQM/IFM/Views/NIDEC/SMT/View_Tool_Management.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace IFM.Views.NIDEC.SMT
+{
+    partial class View_Tool_Management
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View_Tool_Management));
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.bbiSave = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.bbiNew = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiEdit = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiDelete = new DevExpress.XtraBars.BarButtonItem();
+            this.bbiRefresh = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.bbiPrintPreview = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.bar_status = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.bsiRecordsCount = new DevExpress.XtraBars.BarStaticItem();
+            this.gv_data = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.coluser_cd = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colassign_code = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colpriority = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Status = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.comments = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gc_data = new DevExpress.XtraGrid.GridControl();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_data)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gc_data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup3,
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2});
+            this.ribbonPage1.MergeOrder = 0;
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Home";
+            // 
+            // ribbonPageGroup3
+            // 
+            this.ribbonPageGroup3.ItemLinks.Add(this.bbiSave, true);
+            this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+            this.ribbonPageGroup3.Text = "ribbonPageGroup3";
+            // 
+            // bbiSave
+            // 
+            this.bbiSave.Caption = "Save";
+            this.bbiSave.Id = 20;
+            this.bbiSave.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("bbiSave.ImageOptions.Image")));
+            this.bbiSave.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("bbiSave.ImageOptions.LargeImage")));
+            this.bbiSave.Name = "bbiSave";
+            this.bbiSave.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BbiSave_ItemClick);
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.AllowTextClipping = false;
+            this.ribbonPageGroup1.CaptionButtonVisible = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonPageGroup1.ItemLinks.Add(this.bbiNew);
+            this.ribbonPageGroup1.ItemLinks.Add(this.bbiEdit);
+            this.ribbonPageGroup1.ItemLinks.Add(this.bbiDelete);
+            this.ribbonPageGroup1.ItemLinks.Add(this.bbiRefresh);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "Tasks";
+            // 
+            // bbiNew
+            // 
+            this.bbiNew.Caption = "New";
+            this.bbiNew.Id = 16;
+            this.bbiNew.ImageOptions.ImageUri.Uri = "New";
+            this.bbiNew.Name = "bbiNew";
+            this.bbiNew.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BbiNew_ItemClick);
+            // 
+            // bbiEdit
+            // 
+            this.bbiEdit.Caption = "Edit";
+            this.bbiEdit.Id = 17;
+            this.bbiEdit.ImageOptions.ImageUri.Uri = "Edit";
+            this.bbiEdit.Name = "bbiEdit";
+            this.bbiEdit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BbiEdit_ItemClick);
+            // 
+            // bbiDelete
+            // 
+            this.bbiDelete.Caption = "Delete";
+            this.bbiDelete.Id = 18;
+            this.bbiDelete.ImageOptions.ImageUri.Uri = "Delete";
+            this.bbiDelete.Name = "bbiDelete";
+            this.bbiDelete.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BbiDelete_ItemClick);
+            // 
+            // bbiRefresh
+            // 
+            this.bbiRefresh.Caption = "Refresh";
+            this.bbiRefresh.Id = 19;
+            this.bbiRefresh.ImageOptions.ImageUri.Uri = "Refresh";
+            this.bbiRefresh.Name = "bbiRefresh";
+            this.bbiRefresh.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BbiRefresh_ItemClick);
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.AllowTextClipping = false;
+            this.ribbonPageGroup2.CaptionButtonVisible = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonPageGroup2.ItemLinks.Add(this.bbiPrintPreview);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "Print and Export";
+            // 
+            // bbiPrintPreview
+            // 
+            this.bbiPrintPreview.Caption = "Print Preview";
+            this.bbiPrintPreview.Id = 14;
+            this.bbiPrintPreview.ImageOptions.ImageUri.Uri = "Preview";
+            this.bbiPrintPreview.Name = "bbiPrintPreview";
+            this.bbiPrintPreview.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.BbiPrintPreview_ItemClick);
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem,
+            this.ribbonControl.SearchEditItem,
+            this.bbiPrintPreview,
+            this.bbiNew,
+            this.bbiEdit,
+            this.bbiDelete,
+            this.bbiRefresh,
+            this.bbiSave});
+            this.ribbonControl.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl.MaxItemId = 21;
+            this.ribbonControl.Name = "ribbonControl";
+            this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.OfficeUniversal;
+            this.ribbonControl.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.Size = new System.Drawing.Size(781, 79);
+            this.ribbonControl.StatusBar = this.bar_status;
+            this.ribbonControl.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden;
+            // 
+            // bar_status
+            // 
+            this.bar_status.ItemLinks.Add(this.bsiRecordsCount);
+            this.bar_status.Location = new System.Drawing.Point(0, 447);
+            this.bar_status.Name = "bar_status";
+            this.bar_status.Ribbon = this.ribbonControl;
+            this.bar_status.Size = new System.Drawing.Size(781, 31);
+            // 
+            // bsiRecordsCount
+            // 
+            this.bsiRecordsCount.Caption = "RECORDS : 0";
+            this.bsiRecordsCount.Id = 15;
+            this.bsiRecordsCount.Name = "bsiRecordsCount";
+            // 
+            // gv_data
+            // 
+            this.gv_data.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.gv_data.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.coluser_cd,
+            this.colassign_code,
+            this.colpriority,
+            this.Status,
+            this.comments});
+            this.gv_data.GridControl = this.gc_data;
+            this.gv_data.Name = "gv_data";
+            this.gv_data.OptionsBehavior.Editable = false;
+            this.gv_data.OptionsBehavior.ReadOnly = true;
+            this.gv_data.OptionsEditForm.EditFormColumnCount = 2;
+            this.gv_data.OptionsEditForm.ShowOnDoubleClick = DevExpress.Utils.DefaultBoolean.True;
+            // 
+            // coluser_cd
+            // 
+            this.coluser_cd.Caption = "User Code";
+            this.coluser_cd.FieldName = "user_cd";
+            this.coluser_cd.Name = "coluser_cd";
+            this.coluser_cd.Visible = true;
+            this.coluser_cd.VisibleIndex = 0;
+            // 
+            // colassign_code
+            // 
+            this.colassign_code.Caption = "Assign";
+            this.colassign_code.FieldName = "assign_code";
+            this.colassign_code.Name = "colassign_code";
+            this.colassign_code.Visible = true;
+            this.colassign_code.VisibleIndex = 1;
+            // 
+            // colpriority
+            // 
+            this.colpriority.Caption = "Priority";
+            this.colpriority.FieldName = "priority";
+            this.colpriority.Name = "colpriority";
+            this.colpriority.Visible = true;
+            this.colpriority.VisibleIndex = 2;
+            // 
+            // Status
+            // 
+            this.Status.Caption = "Status";
+            this.Status.FieldName = "status";
+            this.Status.Name = "Status";
+            this.Status.Visible = true;
+            this.Status.VisibleIndex = 3;
+            // 
+            // comments
+            // 
+            this.comments.Caption = "Comments";
+            this.comments.FieldName = "comments";
+            this.comments.Name = "comments";
+            this.comments.Visible = true;
+            this.comments.VisibleIndex = 4;
+            // 
+            // gc_data
+            // 
+            this.gc_data.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gc_data.Location = new System.Drawing.Point(0, 79);
+            this.gc_data.MainView = this.gv_data;
+            this.gc_data.MenuManager = this.ribbonControl;
+            this.gc_data.Name = "gc_data";
+            this.gc_data.Size = new System.Drawing.Size(781, 368);
+            this.gc_data.TabIndex = 2;
+            this.gc_data.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gv_data});
+            // 
+            // view_sys_roles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 478);
+            this.Controls.Add(this.gc_data);
+            this.Controls.Add(this.bar_status);
+            this.Controls.Add(this.ribbonControl);
+            this.Name = "view_sys_roles";
+            this.Ribbon = this.ribbonControl;
+            this.StatusBar = this.bar_status;
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_data)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gc_data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+        private DevExpress.XtraBars.BarButtonItem bbiSave;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.BarButtonItem bbiNew;
+        private DevExpress.XtraBars.BarButtonItem bbiEdit;
+        private DevExpress.XtraBars.BarButtonItem bbiDelete;
+        private DevExpress.XtraBars.BarButtonItem bbiRefresh;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.BarButtonItem bbiPrintPreview;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar bar_status;
+        private DevExpress.XtraBars.BarStaticItem bsiRecordsCount;
+        private DevExpress.XtraGrid.Views.Grid.GridView gv_data;
+        private DevExpress.XtraGrid.Columns.GridColumn coluser_cd;
+        private DevExpress.XtraGrid.Columns.GridColumn colassign_code;
+        private DevExpress.XtraGrid.Columns.GridColumn colpriority;
+        private DevExpress.XtraGrid.Columns.GridColumn Status;
+        private DevExpress.XtraGrid.Columns.GridColumn comments;
+        private DevExpress.XtraGrid.GridControl gc_data;
+    }
+}
